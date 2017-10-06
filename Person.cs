dab63f826de72961d871634fb2a45db943a7b94a
awahel <awahel@gmail.com>
2017-10-06 07:28:51 +0200

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,110 @@
+#pragma warning disable 1591
+
+using System;
+using System.Collections.Generic;
+using Bogus.DataSets;
+using Newtonsoft.Json;
+
+namespace Bogus
+{
+    /// <summary>
+    /// Uses Faker to generate a person with contextually relevant fields.
+    /// </summary>
+    public class Person
+    {
+        //context variable to store state from Bogus.Extensions so, they
+        //keep returning the result on each person.
+        internal Dictionary<string, object> context = new Dictionary<string, object>();
+
+        public class CardAddress
+        {
+            public class CardGeo
+            {
+                public double Lat;
+                public double Lng;
+            }
+
+            public string Street;
+            public string Suite;
+            public string City;
+            public string ZipCode;
+            public CardGeo Geo;
+        }
+
+        public class CardCompany
+        {
+            public string Name;
+            public string CatchPhrase;
+            public string Bs;
+        }
+
+        public Person(string locale = "en")
+        {
+            Initialize(locale);
+        }
+
+        protected virtual void Initialize(string locale)
+        {
+            var gname = new Name(locale);
+
+            this.FirstName = gname.FirstName();
+            this.LastName = gname.LastName();
+
+            // var ginternet = new Internet(locale);
+
+            //  this.UserName = ginternet.UserName(this.FirstName, this.LastName);
+            //  this.Email = ginternet.Email(this.FirstName, this.LastName);
+            //  this.Website = ginternet.DomainName();
+            //  this.Avatar = ginternet.Avatar();
+
+            var gdate = new Date() { Locale = locale };
+
+            this.DateOfBirth = gdate.Past(50, DateTime.Now.AddYears(-20));
+
+            var gphone = new PhoneNumbers(locale);
+            this.Phone = gphone.PhoneNumber();
+
+            var gaddress = new Address(locale);
+
+            this.Address = new CardAddress
+                {
+                    Street = gaddress.StreetAddress(),
+                    Suite = gaddress.SecondaryAddress(),
+                    City = gaddress.City(),
+                    ZipCode = gaddress.ZipCode(),
+                    Geo = new CardAddress.CardGeo
+                        {
+                            Lat = gaddress.Latitude(),
+                            Lng = gaddress.Longitude()
+                        }
+                };
+        }
+/*
+            var gcompany = new Company(locale);
+
+            this.Company = new CardCompany
+                {
+                    Name = gcompany.CompanyName(),
+                    CatchPhrase = gcompany.CatchPhrase(),
+                    Bs = gcompany.Bs()
+                };
+
+        }
+            */
+        public string FirstName;
+        public string LastName;
+        public string UserName;
+        public string Avatar;
+        public string Email;
+        public DateTime DateOfBirth;
+        public CardAddress Address;
+        public string Phone;
+        public string Website;
+        public CardCompany Company;
+        
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
---
+++ PeselGenerator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestDataHelper2.Class
+{
+    class PeselGenerator
+    {
+        private readonly Random _random;
+
+        public PeselGenerator()
+        {
+            _random = new Random();
+        }
+
+        public string Generate()
+        {
+            var peselStringBuilder = new StringBuilder();
+            DateTime birthDate = GenerateDate(1900, 2099);
+
+            AppendPeselDate(birthDate, peselStringBuilder);
+
+            peselStringBuilder.Append(GenerateRandomNumbers(4));
+
+            peselStringBuilder.Append(PeselCheckSumCalculator.Calculate(peselStringBuilder.ToString()));
+
+            return peselStringBuilder.ToString();
+        }
+
+        public static string GetPeselMonthShiftedByYear(DateTime date)
+        {
+            if (date.Year < 1900 || date.Year > 2299)
+            {
+                throw new NotSupportedException(string.Format("PESEL for year: {0} is not supported", date.Year));
+            }
+
+            int monthShift = (int)((date.Year - 1900) / 100) * 20;
+
+            return (date.Month + monthShift).ToString("00");
+        }
+
+        private DateTime GenerateDate(int yearFrom, int yearTo)
+        {
+            int year = _random.Next(yearFrom, yearTo + 1);
+            int month = _random.Next(12) + 1;
+            int day = _random.Next(DateTime.DaysInMonth(year, month)) + 1;
+
+            return new DateTime(year, month, day);
+        }
+
+        private void AppendPeselDate(DateTime date, StringBuilder builder)
+        {
+            builder.Append((date.Year % 100).ToString("00"));
+            builder.Append(GetPeselMonthShiftedByYear(date));
+            builder.Append(date.Day.ToString("00"));
+        }
+
+        private string GenerateRandomNumbers(int numbersCount)
+        {
+            int maxValue = (int)Math.Pow(10, numbersCount);
+            string format = "D" + numbersCount;
+
+            return _random.Next(maxValue).ToString(format);
+        }
+    }
+
+    public class PeselValidator
+    {
+        public static bool IsValid(string pesel)
+        {
+            var regex = new Regex("^\\d{11}$");
+
+            if (!regex.IsMatch(pesel))
+            {
+                return false;
+            }
+
+            int checkSum = PeselCheckSumCalculator.Calculate(pesel);
+            int lastDigit = pesel.Last() - '0';
+
+            return lastDigit == checkSum;
+        }
+    }
+
+    public class PeselCheckSumCalculator
+    {
+        private static readonly int[] _Weight = new[] { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+
+        public static int Calculate(string pesel)
+        {
+            int checkSum = pesel.Zip(_Weight, (digit, weight) => (digit - '0') * weight)
+                .Sum();
+
+            int lastDigit = checkSum % 10;
+
+            return lastDigit == 0 ? 0 : 10 - lastDigit;
+        }
+    }
+}
